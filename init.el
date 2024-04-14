6bcb5d0d701c5425d3bcc691a63dad546d43eca6
YKengo1224 <kengo_y_15@icloud.com>
2024-04-14 13:30:48 +0900

    add mwim

--- init.el
+++ init.el
@@ -64,7 +64,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(neotree auto-package-update all-the-icons highlight-indent-guides swiper which-key doom-modeline use-package mozc mew company eglot)))
+   '(mwim neotree auto-package-update all-the-icons highlight-indent-guides swiper which-key doom-modeline use-package mozc mew company eglot)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -204,4 +204,12 @@
   (setq neo-create-file-auto-open t)
   (setq neo-theme (if (display-graphic-p) 'icon 'arrow))
   (bind-key [f8] 'neotree-toggle)
+  )
+
+;;行末、行頭への移動を拡張
+(use-package mwim
+  :ensure t
+  :bind
+  (("C-a" . mwim-beginning-of-code-or-line)
+   ("C-e" . mwim-end-of-code-or-line))
   )
